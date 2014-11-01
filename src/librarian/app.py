c8b3cc2b16895ccbd5d850e21d18926300b839ae
Stephan Richter <stephan.richter@gmail.com>
2014-11-01 12:13:19 -0400

    Some library-like stuff.
    - Set proper HTTP codes.
    - Easy ways to search for stuff and limit it.
    - Server now listens to all interfaces.

--- src/librarian/app.py
+++ src/librarian/app.py
@@ -52,12 +52,37 @@ def result(code, msg, data=None):
         'status_msg': msg}
     if data is not None:
         res.update(data)
-    return jsonify(res)
+    res = jsonify(res)
+    res.status_code = code
+    res.status = '%i %s' %(code, msg)
+    return res
+
+def add_search_params(fields, request, query, qargs):
+    if not fields:
+        return query, qargs
+    field_queries = []
+    for field in fields:
+        value = request.args.get(field)
+        if value is not None:
+            field_queries.append('%s LIKE %%s' % field)
+            qargs += ('%'+value+'%',)
+    if field_queries:
+        query += ' WHERE '
+        query += ' AND '.join(field_queries)
+    return query, qargs
+
+def add_limit_params(request, query, qargs):
+    start = request.args.get('start')
+    if start is not None:
+        count = int(request.args.get('count', 20))
+        query += ' LIMIT %s, %s'
+        qargs += (int(start), count)
+    return query, qargs
 
 def main():
     # Load modules, so that their routes get registered. A bit lame.
-    from librarian import item, borrower
-    app.run(debug=DEBUG)
+    from librarian import item, borrower, checkout
+    app.run(host='0.0.0.0', debug=DEBUG)
 
 if __name__ == '__main__':
     main()
