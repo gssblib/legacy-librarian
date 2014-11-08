9292a008e22c8303fbb727f3719520020d011a43
Stephan Richter <stephan.richter@gmail.com>
2014-11-08 12:07:07 -0500

    Properly implement item returns.

--- src/librarian/static/client/controllers/returns.js
+++ src/librarian/static/client/controllers/returns.js
@@ -3,4 +3,20 @@
  */
 angular.module("library")
 .controller("returnCtrl", function ($scope, $http, $timeout) {
+  $scope.data = {};
+  $scope.data.returnedItems = [];
+  $scope.returnItem = function(barcode) {
+    $http.get('/api/checkin', {params: {barcode: barcode}}).then(
+        function(response) {
+            console.log(response.data);
+            $scope.data.returnedItems.push(response.data);
+            $scope.errorMessage = null;
+        },
+        function(response) {
+            console.log(response);
+            $scope.errorMessage = response.data.status_msg;
+        }
+    );
+  };
+
 });
