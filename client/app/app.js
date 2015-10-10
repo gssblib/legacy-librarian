52fdec73f8e9dea48a6cab8c6f4a6cb2feb85478
Stephan Richter <stephan.richter@gmail.com>
2015-10-10 11:44:23 -0400

    Add ability to export report as CSV.

--- client/app/app.js
+++ client/app/app.js
@@ -2,7 +2,9 @@
  * Main angularjs file. Define the library module and the routes.
  */
 
-angular.module('library', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'dialogs.main', 'lbAuth'],
+angular.module('library', [
+  'ngRoute', 'ngAnimate', 'ui.bootstrap', 'dialogs.main', 'lbAuth',
+  'ngSanitize', 'ngCsv'],
   ['$httpProvider', function ($httpProvider) {
     $httpProvider.interceptors.push(
       ['$location', '$q', '$rootScope', function ($location, $q, $rootScope) {
