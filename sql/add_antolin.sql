2d75b175040a00dbfd86b61b593f8afa4c1bcf12
Stephan Richter <stephan.richter@gmail.com>
2016-10-08 14:11:27 -0400

    Fix SQL.

--- sql/add_antolin.sql
+++ sql/add_antolin.sql
@@ -1,6 +1,6 @@
 -- Adds new antolin table.
 -- Adds the 'antolin_sticker' column to the items table with default false.
---
+
 alter table items
 add column antolin_sticker BOOLEAN
 not null default false;
@@ -8,16 +8,16 @@ not null default false;
 DROP TABLE IF EXISTS antolin;
 CREATE TABLE antolin (
   id INT(11) NOT NULL AUTO_INCREMENT,
-  author VARCHAR,
-  title VARCHAR,
-  publisher VARCHAR,
+  author TEXT,
+  title TEXT,
+  publisher TEXT,
   isbn10 VARCHAR(10),
   isbn10_formatted VARCHAR(13),
   isbn13 VARCHAR(13),
   isbn13_formatted VARCHAR(17),
   book_id INT,
   available_since DATE,
-  grade VARCHAR,
+  grade TEXT,
   num_read INT,
   PRIMARY KEY (id),
   UNIQUE KEY book_id (book_id)
--- sql/schema.sql
+++ sql/schema.sql
@@ -117,16 +117,16 @@ CREATE TABLE users (
 DROP TABLE IF EXISTS antolin;
 CREATE TABLE antolin (
   id INT(11) NOT NULL AUTO_INCREMENT,
-  author VARCHAR,
-  title VARCHAR,
-  publisher VARCHAR,
+  author TEXT,
+  title TEXT,
+  publisher TEXT,
   isbn10 VARCHAR(10),
   isbn10_formatted VARCHAR(13),
   isbn13 VARCHAR(13),
   isbn13_formatted VARCHAR(17),
   book_id INT,
   available_since DATE,
-  grade VARCHAR,
+  grade TEXT,
   num_read INT,
   PRIMARY KEY (id),
   UNIQUE KEY book_id (book_id)
