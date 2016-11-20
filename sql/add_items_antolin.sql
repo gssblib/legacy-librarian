7a51e0a9df15a6d9883a1caf965d100ccf4de764
Andreas Hohmann <ahohmann@server.fake>
2016-11-19 19:53:54 -0500

    remove old update scripts

--- sql/add_items_antolin.sql
+++
@@ -1,6 +0,0 @@
--- Adds the 'antolin' column to the items table with default false.
---
-alter table items
-add column antolin BOOLEAN
-not null default false;
-
--- sql/add_items_expansion.sql
+++
@@ -1,6 +0,0 @@
--- Adds various new fields for better identification and processing.
---
-ALTER TABLE items ADD COLUMN `isbn10` varchar(10);
-ALTER TABLE items ADD COLUMN `isbn13` varchar(13);
-ALTER TABLE items ADD COLUMN `antolin_book_id` int;
-ALTER TABLE items ADD COLUMN `cover` blob;
