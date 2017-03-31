284e4ddf94bc217f91265fa4f115d5438796f4ee
Andreas Hohmann <ahohmann@fake.com>
2017-03-31 18:55:39 -0400

    added primeng menu bar

--- client2/src/app/app.component.ts
+++ client2/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import "rxjs/add/operator/catch";
 import "rxjs/add/operator/map";
+import { MenuItem } from "primeng/primeng";
 
 @Component({
   selector: 'gsl-root',
@@ -8,11 +9,28 @@ import "rxjs/add/operator/map";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  private items: MenuItem[];
   title = 'app works!';
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Items',
+        items: [
+          {
+            label: 'Search',
+            routerLink: ['/items']
+          },
+          {
+            label: 'New',
+            icon: 'fa-edit',
+            url: 'http://www.heise.de/'
+          }
+        ]
+      }
+    ];
   }
 }
--- client2/src/app/app.module.ts
+++ client2/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
 import { ItemsModule } from "./items/items.module";
 import { AppRoutingModule } from "./app-routing.module";
+import { MenubarModule } from "primeng/primeng";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { AppRoutingModule } from "./app-routing.module";
     HttpModule,
     CoreModule,
     AppRoutingModule,
-    ItemsModule
+    ItemsModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
