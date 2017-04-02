dd2ce4548ac311308fe667732de7295a390f9aaa
Andreas Hohmann <ahohmann@fake.com>
2017-04-02 10:57:41 -0400

    basic home page

--- client2/src/app/items/items-routing.module.ts
+++ client2/src/app/items/items-routing.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { ItemSearchComponent } from "./item-search/item-search.component";
 import { ItemPageComponent } from "./item-page/item-page.component";
+import { ReturnPageComponent } from "./return-page/return-page.component";
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       { path: 'items/:id', component: ItemPageComponent },
-      { path: 'items', component: ItemSearchComponent }
+      { path: 'items', component: ItemSearchComponent },
+      { path: 'return', component: ReturnPageComponent },
     ])
   ],
   exports: [
