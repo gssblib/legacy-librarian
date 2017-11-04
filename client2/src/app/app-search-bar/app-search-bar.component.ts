44310faf73dac5fbb808ec60faec559e0550f24c
Stephan Richter <stephan.richter@gmail.com>
2017-11-04 15:40:06 -0400

    Implement a multi-entity global search.

---
+++ client2/src/app/app-search-bar/app-search-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gsl-app-search-bar',
+  templateUrl: './app-search-bar.component.html',
+  styleUrls: ['./app-search-bar.component.css']
+})
+export class AppSearchBarComponent implements OnInit {
+  type = 'item';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
--- client2/src/app/app.module.ts
+++ client2/src/app/app.module.ts
@@ -6,17 +6,26 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ItemsModule } from './items/items.module';
 import { AppRoutingModule } from './app-routing.module';
-import { MatIconModule, MatListModule, MatSidenavModule, MatSnackBarModule, MatToolbarModule } from '@angular/material';
+import {
+  MatIconModule,
+  MatListModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSnackBarModule,
+  MatToolbarModule
+} from '@angular/material';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { BorrowersModule } from './borrowers/borrowers.module';
 import { HttpClientModule } from "@angular/common/http";
+import { AppSearchBarComponent } from './app-search-bar/app-search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NotFoundComponent,
+    AppSearchBarComponent,
   ],
   imports: [
     BrowserModule,
@@ -26,6 +35,7 @@ import { HttpClientModule } from "@angular/common/http";
     ItemsModule,
     BorrowersModule,
     AppRoutingModule,
+    MatSelectModule,
     MatSnackBarModule,
     MatSidenavModule,
     MatToolbarModule,
