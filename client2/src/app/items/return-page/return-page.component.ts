413634baa404a12aab4d3d030defcd1033d327bc
Stephan Richter <stephan.richter@gmail.com>
2017-11-08 12:40:38 -0500

    Make styling improvements plus better item link.

--- client2/src/app/items/return-page/return-page.component.ts
+++ client2/src/app/items/return-page/return-page.component.ts
@@ -49,6 +49,7 @@ export class ReturnPageComponent implements OnInit {
   }
 
   private onError(barcode: string, error: RpcError) {
+    console.log(error);
     this.errorService.showError(this.toErrorMessage(barcode, error));
     this.barcode.barcode = '';
     return null;
