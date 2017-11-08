5c03143213e48a353702aaa38c9bdb4d861c1020
Stephan Richter <stephan.richter@gmail.com>
2017-11-08 16:43:02 -0500

    Proper error handling on checkouts.

--- client2/src/app/borrowers/borrower-checkouts/borrower-checkouts.component.ts
+++ client2/src/app/borrowers/borrower-checkouts/borrower-checkouts.component.ts
@@ -1,4 +1,8 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ErrorService } from "../../core/error-service";
+import { RpcError } from "../../core/rpc-error";
+import { BarcodeFieldComponent } from "../../shared/barcode-field/barcode-field.component";
 import { Borrower } from '../shared/borrower';
 import { BorrowersService } from '../shared/borrowers.service';
 import { BorrowerService } from '../shared/borrower.service';
@@ -17,9 +21,14 @@ export class BorrowerCheckoutsComponent implements OnInit {
   @Output()
   borrowerChange: EventEmitter<any> = new EventEmitter();
 
-  constructor(private borrowerService: BorrowerService,
-              private borrowersService: BorrowersService) {
-  }
+  @ViewChild('barcode')
+  barcode: BarcodeFieldComponent;
+
+  constructor(
+    private errorService: ErrorService,
+    private borrowerService: BorrowerService,
+    private borrowersService: BorrowersService
+  ) { }
 
   ngOnInit() {
     this.borrower = this.borrowerService.borrower;
@@ -27,10 +36,32 @@ export class BorrowerCheckoutsComponent implements OnInit {
 
   checkout(barcode) {
     console.log('checkout ' + JSON.stringify(barcode));
-    this.borrowersService.checkOutItem(barcode, this.borrower.borrowernumber).subscribe(
-      result => {
-        this.borrowerChange.emit(null);
-      }
-    );
+    this.borrowersService.checkOutItem(barcode, this.borrower.borrowernumber)
+      .catch((error: RpcError) => this.onError(barcode, error))
+        .subscribe((barcode: string) => this.onSuccess(barcode));
+  }
+
+  private onSuccess(result) {
+    console.log('success', result);
+    this.borrowerChange.emit(null);
+    this.barcode.barcode = '';
+  }
+
+  private onError(barcode: string, error: RpcError) {
+    this.errorService.showError(this.toErrorMessage(barcode, error));
+    this.barcode.barcode = '';
+    // Resolve the error.
+    return Observable.create(() => {});
+  }
+
+  private toErrorMessage(barcode: string, error: RpcError) {
+    switch (error.errorCode) {
+      case 'ENTITY_NOT_FOUND':
+        return `Item with barcode ${barcode} does not exist.`;
+      case 'ITEM_ALREADY_CHECKED_OUT':
+        return `Item with barcode ${barcode} is already checked out.`;
+      default:
+        return `Server error: ${error.errorCode}`;
+    }
   }
 }
