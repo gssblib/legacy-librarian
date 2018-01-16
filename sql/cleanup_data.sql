d708f91d618524089fe69afa6add8b1f5ecb0db5
Stephan Richter <stephan.richter@gmail.com>
2018-01-16 09:51:19 -0500

    Massive cleanup of the classifications and some series info.
    Andreas, please review with Yolyett and apply.

--- sql/cleanup_data.sql
+++ sql/cleanup_data.sql
@@ -169,6 +169,356 @@ UPDATE items SET classification = left(author, 3) WHERE subject LIKE 'Erz%' AND 
 --  * Convert classification "[A-Z]{3}" to first 3 letters of author effectively titling the string..
 UPDATE items SET classification = left(author, 3) WHERE subject LIKE 'Erz%' AND classification REGEXP BINARY '[A-Z]{3}';
 
--- TODO
--- Maerchen classifications.
+-- Comic classification fixes
+UPDATE items SET classification = 'Raetsel' WHERE subject LIKE 'Comic%' and classification = 'Rae Raetsel';
+UPDATE items SET classification = 'Comic' WHERE subject LIKE 'Comic%' and classification = 'Comic book';
+UPDATE items SET classification = 'Comic' WHERE subject LIKE 'Comic%' and classification = 'na';
+UPDATE items SET
+    classification = 'Comic',
+    category = 'Comic'
+  WHERE subject LIKE 'Comic%' and classification = 'Buch in Comic';
+UPDATE items SET
+    classification = 'Comic',
+    category = 'Comic'
+  WHERE subject LIKE 'Comic%' and classification = 'Comic Sachkunde';
+UPDATE items SET
+    category = 'Comic',
+    classification = 'Witze',
+    seriestitle = NULL
+  WHERE barcode = '000010800';
+UPDATE items SET
+    classification = 'Comic-Roman',
+    seriestitle = NULL
+  WHERE barcode = '000010778';
+
+-- Lustiges Taschenbuch fixes.
+UPDATE items SET
+    title = 'Lustiges Taschenbuch - Die Wikinger Expedition',
+    classification = 'Comic'
+  WHERE barcode = '000004024';
+UPDATE items SET
+    title = 'Lustiges Taschenbuch - Mammut',
+    classification = 'Comic'
+  WHERE barcode = '000005193';
+UPDATE items SET
+    title = 'Lustiges Taschenbuch - Phantomias ist wieder da!',
+    category = 'Comic',
+    age = 'K-2'
+  WHERE barcode = '000010503';
+UPDATE items SET
+    title = 'Lustiges Taschenbuch - Taschenbuch Auf Schatzsuche',
+    category = 'Comic',
+    seriestitle = 'Lustiges Taschenbuch',
+    age = 'K-2'
+  WHERE barcode = '000010505';
+UPDATE items SET
+    title = 'Lustiges Taschenbuch 333'
+  WHERE barcode = '000010805';
+UPDATE items SET
+    category = 'Comic',
+    seriestitle = 'Lustiges Taschenbuch'
+  WHERE barcode = '000012793';
+UPDATE items SET author = 'Disney, Walt' WHERE barcode = '000012793';
+UPDATE items SET age = 'K-2' WHERE barcode = '000001350';
+UPDATE items SET age = 'K-2' WHERE barcode = '000001350';
+UPDATE items SET category = 'Comic' WHERE barcode = '000010517';
+UPDATE items SET
+    title = concat('Lustiges Taschenbuch - ', title),
+    author = 'Disney, Walt',
+    classification = 'Comic'
+  WHERE subject LIKE 'Comic%' and classification LIKE 'Series';
+
+-- Donald Duck fixes.
+UPDATE items SET
+    classification = 'Comic',
+    author = 'Disney, Walt'
+  WHERE barcode = '000014296';
+
+-- Peanuts fixes.
+UPDATE items SET
+    category = 'Comic',
+    classification = 'Comic'
+  WHERE barcode = '000008789';
+UPDATE items SET
+    category = 'Comic',
+    classification = 'Comic'
+  WHERE barcode = '000012794';
+
+-- Lucky Luke fixes.
+UPDATE items SET classification = 'Comic'
+  WHERE subject LIKE 'Comic%' and title LIKE '%Lucky Luke%';
+UPDATE items SET
+    title = concat('Lucky Luke - ', title),
+    classification = 'Comic'
+  WHERE subject LIKE 'Comic%' and classification = 'Comic Lucky Luke';
+UPDATE items SET
+    category = 'Comic',
+    seriestitle = 'Lucky Luke'
+  WHERE barcode = '000013074';
+
+-- Gaston fixes.
+UPDATE items SET
+    classification = 'Comic'
+  WHERE subject LIKE 'Comic%' and seriestitle = 'Gaston';
+
+-- Welt des Wissens classification fixes.
+UPDATE items SET
+    classification = 'Serie',
+    seriestitle = 'Welt des Wissens',
+    subject = 'Sachkunde Serie - hellblau'
+  WHERE barcode = '000011108';
+
+
+-- Sachkunde fixes.
+UPDATE items SET
+    classification = 'N - Natur / Umwelt / Jahreszeiten'
+  WHERE barcode = '000010456';
+UPDATE items SET
+    classification = 'Le - Lernen / Lexikon'
+  WHERE barcode IN ('000014196', '000011743');
+UPDATE items SET
+    classification = 'G - Geschichte / Kulturen'
+  WHERE barcode = '000004666';
+UPDATE items SET
+    subject = 'DVD'
+  WHERE barcode = '000006691';
+
+
+-- Zeitschriften
+UPDATE items SET
+    classification = 'Zeitschrift',
+    age = 'K-2',
+    author = 'na, na',
+    category = 'Zeitschrift'
+  WHERE subject LIKE 'Zeitschrift%' AND seriestitle LIKE 'Maedchen';
+UPDATE items SET
+    classification = 'Zeitschrift',
+    age = 'K-2',
+    author = 'na, na'
+  WHERE subject LIKE 'Zeitschrift%' AND seriestitle LIKE 'Dein Spiegel';
+UPDATE items SET
+    classification = 'Zeitschrift',
+    age = 'K-2',
+    author = 'na, na',
+    seriestitle = 'Dein Spiegel'
+  WHERE barcode IN ('000010526', '000011875', '000011823');
+UPDATE items SET
+    classification = 'Zeitschrift',
+    age = 'K-2',
+    author = 'na, na',
+    category = 'Zeitschrift',
+    seriestitle = 'GEOlino'
+  WHERE subject LIKE 'Zeitschrift%' AND seriestitle = 'geolino';
+UPDATE items SET
+    classification = 'Zeitschrift',
+    age = 'K-2',
+    author = 'na, na',
+    category = 'Zeitschrift',
+    seriestitle = 'GEOlino'
+  WHERE barcode IN ('000010565', '000010753');
+UPDATE items SET
+    author = 'na, na',
+    seriestitle = 'GEOlino'
+  WHERE subject LIKE 'Zeitschrift%' AND seriestitle = 'GeoLino 2012';
+UPDATE items SET
+    classification = 'Zeitschrift'
+  WHERE subject LIKE 'Zeitschrift%' AND seriestitle = 'National Geographic Kids';
+UPDATE items SET
+    age = 'K-2'
+  WHERE subject LIKE 'Zeitschrift%' AND seriestitle = 'National Geographic';
+UPDATE items SET
+    author = 'na, na'
+  WHERE subject LIKE 'Zeitschrift%' AND seriestitle = 'Star Wars';
+
 -- Bilderbuch classifications.
+UPDATE items SET
+    classification = 'Ze - Zeit'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Zeit', 'Ze Zeit Bilderbuch');
+UPDATE items SET
+    classification = 'Za - Zahlen'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Zahlen');
+UPDATE items SET
+    classification = 'Ve - Verkehr / Fahrzeuge'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN (
+      'Verkehr', 'VE Fahrzeuge/Verkehr', 'Ve - Verkehr',
+      'Ve - Fahrzuge/Verkehr', 'Ve');
+UPDATE items SET
+    classification = 'Ti - Tiere'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Kleiner Braunbaer', 'Kaninchen');
+UPDATE items SET
+    classification = 'So - Sozialverhalten'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('So - Sozialverthalten');
+UPDATE items SET
+    classification = 'Serie Mey Meyers Kleine Kinderbibliothek'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN (
+      'Serie Mey Meyers Kleine Kinderbibliotek',
+      'Serie Meyers Kleine Kinderbibliothek',
+      'Seirie Meyers kleine Kinderbibliothek');
+UPDATE items SET
+    classification = 'Serie Lesemaus'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Lesemaus', 'Lesemaus Serie', 'Bilderbuch/Serie/Lesemaus');
+UPDATE items SET
+    classification = 'Serie Lesemaus Conni'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Serie Lesemaus/Maxi/Conni', 'Conni Lesemaus serie');
+UPDATE items SET
+    classification = 'Serie Kokosnuss'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Serie SIE Kokosnuss', 'Kokosnuss Serie');
+UPDATE items SET
+    classification = 'Serie Disney'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Serie DIS Disney', 'Serie DIS');
+UPDATE items SET
+    classification = 'Serie Die Maus'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Die Maus Serie');
+UPDATE items SET
+    classification = 'Sch - Schule'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Schule', 'Sch Schule');
+UPDATE items SET
+    classification = 'N - Natur / Umwelt / Jahreszeiten'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Nat - Natur', 'Nat', 'Schmetterling', 'berg');
+UPDATE items SET
+    classification = 'Far - Farben'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Farben', 'Far', 'Far Farben', 'Bilderbuch/Farben');
+UPDATE items SET
+    classification = 'Fam - Familie'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Familie', 'Fam Familie', 'Fam -Familie', 'Fam', 'Bilderbuch/Familie');
+UPDATE items SET
+    classification = 'Dis - Disney'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('DIS Disney', 'Disney');
+UPDATE items SET
+    classification = 'Bb - Boardbook'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Bb', 'Bb Boardbook', 'Bb Boarbook', 'Boardbook');
+UPDATE items SET
+    classification = 'B/G'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('B/G Bilderbuch gross', 'Bilderbuch gross', 'Bilderbuch/gross', 'BG');
+UPDATE items SET
+    classification = 'B/K'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('B/K Bilderbuch Klein', 'Bilderbuch klein');
+UPDATE items SET
+    classification = 'B/M'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('B/M Bilderbuch mittel', 'Bilderbuch mittel');
+UPDATE items SET
+    classification = 'Bau - Bauernhof'
+  WHERE
+    subject LIKE 'Bilderbuch%' AND
+    classification IN ('Bau');
+UPDATE items SET
+    classification = 'B/A Lin'
+  WHERE barcode = '000013033';
+UPDATE items SET
+    classification = 'B/A Lio'
+  WHERE barcode = '000005689';
+UPDATE items SET
+    classification = 'B/K/A Lan'
+  WHERE barcode = '000007888';
+UPDATE items SET
+    classification = 'B/K/A Bon'
+  WHERE barcode = '000012384';
+UPDATE items SET
+    classification = 'Serie Mey Meyers Kleine Kinderbibliothek'
+  WHERE barcode = '000000611';
+UPDATE items SET
+    classification = 'Serie Licht an'
+  WHERE barcode = '000000687';
+UPDATE items SET
+    classification = 'Serie Licht an'
+  WHERE barcode = '000011554';
+UPDATE items SET
+    title = 'Kennst Du Das? - Bauernhof',
+    classification = 'Serie Duden',
+    seriestitle = 'Kennst Du Das?'
+  WHERE barcode = '000001495';
+UPDATE items SET
+    title = 'Kennst Du Das? - Die Haustiere',
+    classification = 'Serie Duden',
+    seriestitle = 'Kennst Du Das?'
+  WHERE barcode = '000012308';
+UPDATE items SET
+    title = 'Kennst Du Das? - Die Farben',
+    classification = 'Serie Duden',
+    seriestitle = 'Kennst Du Das?'
+  WHERE barcode = '000013563';
+UPDATE items SET
+    classification = 'Dis - Disney'
+  WHERE barcode = '000004393';
+UPDATE items SET
+    classification = 'Serie Licht an',
+    seriestitle = 'Licht an'
+  WHERE barcode = '000003107';
+UPDATE items SET
+    classification = 'So - Sozialverhalten',
+    seriestitle = NULL
+  WHERE barcode = '000011624';
+UPDATE items SET
+    classification = 'Serie'
+  WHERE barcode = '000004425';
+UPDATE items SET
+    age = 'All Ages',
+    classification = 'Serie',
+    author = 'na, na'
+  WHERE barcode = '000004344';
+UPDATE items SET
+    classification = 'Ti - Tiere',
+    seriestitle = NULL
+  WHERE barcode = '000009200';
+
+-- Maerchen classifications
+UPDATE items SET
+    classification = 'P - Poesie'
+  WHERE
+    subject LIKE 'Mae%' AND
+    classification IN ('Poesie', 'P Poesie', 'Poesie/Reime');
+UPDATE items SET
+    classification = 'Maerchen'
+  WHERE
+    subject LIKE 'Mae%' AND
+    classification IN ('Ma - Maerchen', 'Mae', 'Märchen', 'Mae Maerchen', 'na', 'V Vorlesegeschichten');
+UPDATE items SET
+    classification = 'Sagen / Mythen'
+  WHERE
+    subject LIKE 'Mae%' AND
+    classification IN ('Sagen/Mythen', 'Mae Maerchen Sagen/Mythen', 'Sagen Mythen', '2');
+UPDATE items SET
+    classification = 'Hoergeschichten'
+  WHERE
+    subject LIKE 'Mae%' AND
+    classification IN ('geschichten');
