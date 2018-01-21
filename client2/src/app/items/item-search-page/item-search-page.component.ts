470b17938316bd74a8744899ffa8a9523933919f
Andreas Hohmann <ahohmann@fake.com>
2018-01-20 22:55:56 -0500

    migrate item search result table to material data table

--- client2/src/app/items/item-search-page/item-search-page.component.ts
+++ client2/src/app/items/item-search-page/item-search-page.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnDestroy, OnInit, ViewChild } from "@angular/core";
-import { ActivatedRoute, Params, Router } from "@angular/router";
-import { NotificationService } from "../../core/notification-service";
-import { Item } from "../shared/item";
-import { TableFetchResult } from "../../core/table-fetcher";
-import {
-  IPageChangeEvent, ITdDataTableColumn, ITdDataTableSortChangeEvent,
-  TdPagingBarComponent
-} from "@covalent/core";
-import { ParamsUtil } from "../../core/params-util";
-import { SortKey } from "../../core/sort-key";
-import { Subscription } from "rxjs/Subscription";
-import { ItemState } from "../shared/item-state";
-import { ItemsService } from "../shared/items.service";
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { NotificationService } from '../../core/notification-service';
+import { Item } from '../shared/item';
+import { TableFetchResult } from '../../core/table-fetcher';
+import { ParamsUtil } from '../../core/params-util';
+import { SortKey } from '../../core/sort-key';
+import { Subscription } from 'rxjs/Subscription';
+import { ItemState } from '../shared/item-state';
+import { ItemsService } from '../shared/items.service';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { merge } from 'rxjs/observable/merge';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { of as observableOf } from 'rxjs/observable/of';
 
 /**
  * Item search page with search form and result table.
@@ -24,7 +24,7 @@ import { ItemsService } from "../shared/items.service";
   templateUrl: './item-search-page.component.html',
   styleUrls: ['./item-search-page.component.css']
 })
-export class ItemSearchPageComponent implements OnInit, OnDestroy {
+export class ItemSearchPageComponent implements OnInit, AfterViewInit, OnDestroy {
   ItemState = ItemState;
 
   searchFields: string[] = ['title', 'author', 'category', 'subject', 'state', 'seriestitle'];
@@ -38,30 +38,14 @@ export class ItemSearchPageComponent implements OnInit, OnDestroy {
   /** Additional criteria to apply before the search is executed. */
   extraCriteria: Object = {};
 
-  /** Current page number. Set from the URL and the pagination bar. */
-  page: number = 1;
+  displayedColumns = ['barcode', 'status', 'title', 'author', 'subject', 'category'];
+  dataSource = new MatTableDataSource();
 
-  /** Current page size. Set from the URL and the pagination bar. */
-  pageSize: number = 10;
+  resultsLength = 0;
+  isLoadingResults = false;
 
-  /** Current sort key. Set from the URL and table sort event. */
-  sortKey = new SortKey('title', 'ASC');
-
-  @ViewChild('pagingBar')
-  pagingBar: TdPagingBarComponent;
-
-  /** Meta-data for the items table. */
-  columns: ITdDataTableColumn[] = [
-    {name: 'barcode', label: 'Barcode', width: 100, sortable: true},
-    {name: 'status', label: 'Status', width: 140, sortable: false, format: value => ItemState[value]},
-    {name: 'title', label: 'Title', sortable: true},
-    {name: 'author', label: 'Author', width: 220, sortable: true},
-    {name: 'subject', label: 'Subject', width: 180, sortable: true},
-    {name: 'category', label: 'Category', width: 100, sortable: true},
-  ];
-
-  /** Flag set while setting page in paging bar with navigateToPage. */
-  private navigating = false;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   private routeSubscription: Subscription;
 
@@ -83,30 +67,37 @@ export class ItemSearchPageComponent implements OnInit, OnDestroy {
 
   onQueryParamsChanged(params: Params) {
     this.parseParams(params);
-    // TdPagingBar's `page` property is read-only. It can only be changed by calling
-    // the navigateToPage method. We do that if the page differs, but temporarily set
-    // the `navigating` flag to that the page change event triggered by navigateToPage
-    // does not cause another navigation.
-    if (this.pagingBar && this.pagingBar.page !== this.page) {
-      this.navigating = true;
-      this.pagingBar.navigateToPage(this.page);
-    }
-    this.reload();
   }
 
-  onSort(event: ITdDataTableSortChangeEvent) {
-    this.sortKey = SortKey.fromChange(event);
-    this.navigate();
+  sortOrder(sort: MatSort): string {
+    return (sort.direction === 'desc' ? '-' : '') + sort.active;
   }
 
-  onPage(event: IPageChangeEvent) {
-    if (this.navigating) {
-      this.navigating = false;
-    } else {
-      this.page = event.page;
-      this.pageSize = event.pageSize;
-      this.navigate();
-    }
+  ngAfterViewInit(): void {
+    // Reset page when sort order is change.
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+    // Reload data When sort order or page changes.
+    merge(this.sort.sortChange, this.paginator.page, this.route.queryParams)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults = true;
+          const offset = this.paginator.pageIndex * this.paginator.pageSize;
+          const criteria = Object.assign(
+            {}, this.criteria, this.extraCriteria, {'_order': this.sortOrder(this.sort)});
+          return this.itemsService.getItems(criteria, offset, this.paginator.pageSize, true);
+        }),
+        map(result => {
+          this.isLoadingResults = false;
+          this.resultsLength = result.count;
+          return result.rows;
+        }),
+        catchError(() => {
+          this.isLoadingResults = false;
+          return observableOf([]);
+        })
+      ).subscribe(data => this.dataSource.data = data);
   }
 
   onSearch(event) {
@@ -123,11 +114,13 @@ export class ItemSearchPageComponent implements OnInit, OnDestroy {
    */
   private parseParams(params: Params) {
     const p = new ParamsUtil(params);
-    this.page = p.getNumber('page', 1);
-    this.pageSize = p.getNumber('pageSize', 10);
-    this.sortKey = params['order']
+    this.paginator.pageIndex = p.getNumber('page', 1);
+    this.paginator.pageSize = p.getNumber('pageSize', 10);
+    const sortKey = params['order']
       ? SortKey.fromString(params['order'])
       : new SortKey('title', 'ASC');
+    this.sort.active = sortKey.name;
+    this.sort.direction = sortKey.order === 'ASC' ? 'asc' : 'desc';
     this.criteria = p.getValues(this.searchFields);
   }
 
@@ -136,9 +129,9 @@ export class ItemSearchPageComponent implements OnInit, OnDestroy {
    */
   private toQueryParams(): Params {
     return Object.assign({}, this.criteria, {
-      page: this.page,
-      pageSize: this.pageSize,
-      order: this.sortKey.toString(),
+      page: this.paginator.pageIndex,
+      pageSize: this.paginator.pageSize,
+      order: this.sortOrder(this.sort),
     });
   }
 
@@ -152,19 +145,5 @@ export class ItemSearchPageComponent implements OnInit, OnDestroy {
     }).catch(err => {
       this.notificationService.showError('navigation error', err);
     });
-  }
-
-  /**
-   * Gets the items from the server.
-   */
-  private reload() {
-    const offset = (this.page - 1) * this.pageSize;
-    const criteria = Object.assign(
-      {}, this.criteria, this.extraCriteria, {'_order': this.sortKey.toString()});
-    this.itemsService.getItems(criteria, offset, this.pageSize, true).subscribe(
-      result => {
-        this.result = result;
-      }
-    );
   }
 }
