60279ab3b3149b6783c867a3c67b1b943b0db369
ahohmann <ahohmann@users.noreply.github.com>
2020-10-03 15:56:52 -0400

    remove unused component

--- client2/src/app/public/app-public.module.ts
+++ client2/src/app/public/app-public.module.ts
@@ -7,7 +7,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotFoundModule } from '../../not-found/not-found.module';
 import { HomeModule } from '../../home/home.module';
 import { CoreModule } from '../core/core.module';
-import { AppSearchBarComponent } from './app-search-bar/app-search-bar.component';
 import { ItemsModule } from '../items/items.module';
 import { BorrowersModule } from '../borrowers/borrowers.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -65,16 +64,15 @@ import { MatTableModule } from "@angular/material/table";
   declarations: [
     AppPublicComponent,
     ItemBrowserPageComponent,
-    AppSearchBarComponent,
     CheckedOutPageComponent,
     CatalogItemCardComponent,
     CatalogOrderPageComponent,
     CatalogOrderComponent,
   ],
-  providers: [
-  ],
+  providers: [],
   bootstrap: [
     AppPublicComponent
   ]
 })
-export class AppPublicModule { }
+export class AppPublicModule {
+}
