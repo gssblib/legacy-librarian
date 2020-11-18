cba4e38f9b449bf5287337f947cd3df47dc31b3a
Kai Bolay <kbolay@google.com>
2020-11-17 22:49:05 -0500

    Use 'gssblib-server' as 'api' function.

---
+++ firebase/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR=`dirname $0`
+DIR=`cd ${DIR}; pwd`
+
+echo "Deploying for ${DIR}"
+
+cd ${DIR}/functions
+npm install
+
+export NODE_CONFIG_DIR=${DIR}/functions/config
+export NODE_ENV=production
+
+echo "Using ${NODE_CONFIG_DIR}/${NODE_ENV}.json"
+
+# TODO(kbolay): remove "--only functions"
+firebase deploy --only functions
